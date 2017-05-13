c2a462277b5e778b8960301328bc5c0168888d0d
destroyer3104 <misumi3104@gmail.com>
2017-05-14 01:51:25 +0900

    add new file

---
+++ main.py
@@ -0,0 +1,18 @@
+import logging
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
